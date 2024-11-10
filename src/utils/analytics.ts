6151b3b58797b00f5f6f0905696bbe351df0ec2c
manuelpimentel <manuel.pimentel@gpnauticals.com>
2024-11-10 21:34:56 +0100

    Add Google analytics

---
+++ src/utils/analytics.ts
@@ -0,0 +1,12 @@
+// src/utils/analytics.ts
+import ReactGA from "react-ga4";
+
+const measurementId = "G-3V7D9LZQ3X";  // Replace with your Measurement ID
+
+export const initializeAnalytics = () => {
+  ReactGA.initialize(measurementId);
+};
+
+export const logPageView = (path: string) => {
+  ReactGA.send({ hitType: "pageview", page: path });
+};
